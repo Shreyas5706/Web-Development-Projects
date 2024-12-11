c7c949941f3f48c280ce6f7b953ede5dee6f766c
Shreyas5706 <157724917+Shreyas5706@users.noreply.github.com>
2024-12-11 10:57:15 +0530

    New Chrome home page project
    Chrome default new tab clone

--- Back End Projects/Record Generator/main.js
+++ Back End Projects/Record Generator/main.js
@@ -11,17 +11,17 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(root, 'Project.html'));
 });
 
-
+ 
 app.post('/save-to-databse', (req, res) => {
     const records = req.body;
-
+ 
     if (records && records.length > 0) {
         require('./index.mongodb.js').SavetoDatabase(records);  // Call the function to save to MongoDB
         res.json({ "message": 'Records saved successfully!' });
     }
     else {
         res.status(400).json({ "message": 'No records provided.' });
-    }
+    } 
 });
 
 app.listen(port, () => {
